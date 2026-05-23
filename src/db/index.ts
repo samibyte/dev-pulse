346bcdaaa8f6a043c333f774c129446520ebc4a6
samibyte <samibyte.web@gmail.com>
2026-05-23 16:42:17 +0600

    refactor: improve formatting and readability of database table creation queries

--- src/db/index.ts
+++ src/db/index.ts
@@ -15,23 +15,45 @@ export const initDB = async () => {
 
   try {
     await pool.query(`
-          CREATE TABLE IF NOT EXISTS users(
-            id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      CREATE TABLE IF NOT EXISTS users(
+          id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 
-            name VARCHAR(30) NOT NULL,
+          name VARCHAR(30) NOT NULL,
 
-            email VARCHAR(255) UNIQUE NOT NULL,
+          email VARCHAR(255) UNIQUE NOT NULL,
 
-            password TEXT NOT NULL,
+          password TEXT NOT NULL,
 
-            role VARCHAR(15) NOT NULL
-                CHECK (role IN ('maintainer', 'contributor'))
-                DEFAULT 'contributor',
+          role VARCHAR(15) NOT NULL DEFAULT 'contributor'
+              CHECK (role IN ('maintainer', 'contributor')),
 
-            created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-            updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-        )
-      `);
+          created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+          updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+      )
+  `);
+
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS issues(
+          id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+
+          title VARCHAR(150) NOT NULL,
+
+          description TEXT NOT NULL
+              CHECK (CHAR_LENGTH(description) >= 20),
+
+          type VARCHAR(15) NOT NULL
+              CHECK (type IN ('bug', 'feature_request')),
+
+          status VARCHAR(15) NOT NULL DEFAULT 'open'
+              CHECK (status IN ('open', 'in_progress', 'resolved')),
+
+          reporter_id INTEGER NOT NULL,
+
+          created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+          updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+      )
+  `);
+
     console.log("Database connected successfully!");
   } catch (error) {
     console.error("Database initialization failed:", error);
